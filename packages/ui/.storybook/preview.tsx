026c3323349acd6137dd25f1c9153815abada257
samithaf <fernando.samitha@gmail.com>
2023-07-12 13:40:59 +1000

    added theme switcher to storybook

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,8 +1,32 @@
 import type { Preview } from '@storybook/react';
 import './global.css';
-import React from 'react';
+import * as React from 'react';
+
+const withThemeProvider = (Story, context) => {
+  const theme = context.globals?.theme || 'WBC';
+  return (
+    <div data-theme={theme.toLowerCase()}>
+      <Story />
+    </div>
+  );
+};
 
 const preview: Preview = {
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      defaultValue: 'WBC',
+      toolbar: {
+        // The label to show for this toolbar item
+        title: 'Theme',
+        icon: 'paintbrush',
+        // Array of plain string values or MenuItem shape (see below)
+        items: ['BOM', 'BSA', 'BTFG', 'RAMS', 'STG', 'WBC', 'WBG'],
+        // Change title based on selected value
+        dynamicTitle: true,
+      },
+    },
+  },
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
@@ -12,11 +36,7 @@ const preview: Preview = {
       },
     },
   },
-  decorators: [
-    story => {
-      return <div data-theme="wbc">{story()}</div>;
-    },
-  ],
+  decorators: [withThemeProvider],
 };
 
 export default preview;
