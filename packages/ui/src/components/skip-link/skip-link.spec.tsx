01ce6bc73c54d580be569ec31bf742025c78f6c1
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2023-07-19 10:30:42 +1000

    feat(a11y): adding test to test the styles

--- packages/ui/src/components/skip-link/skip-link.spec.tsx
+++ packages/ui/src/components/skip-link/skip-link.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 
 import { SkipLink } from './skip-link.component.js';
+import { styles } from './skip-link.styles.js';
 
 describe('SkipLink', () => {
   it('renders the component', () => {
@@ -10,5 +11,11 @@ describe('SkipLink', () => {
       </SkipLink>,
     );
     expect(container).toBeInTheDocument();
+  });
+  it('renders the style correctly', () => {
+    const style = styles();
+    expect(style).toBe(
+      'sr-only focus:not-sr-only focus:fixed focus:inset-x-0 focus:top-0 focus:bg-white focus:p-3 focus:text-center focus:text-xl focus:text-link focus:underline focus:-outline-offset-2',
+    );
   });
 });
