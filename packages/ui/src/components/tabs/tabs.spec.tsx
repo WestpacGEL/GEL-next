fbbff9c19693bec529c3d1f1c702d00841679b80
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-07-28 16:33:02 +1000

    fix(tabs): test

--- packages/ui/src/components/tabs/tabs.spec.tsx
+++ packages/ui/src/components/tabs/tabs.spec.tsx
@@ -5,12 +5,25 @@ import { styles } from './tabs.styles.js';
 
 describe('Tabs', () => {
   it('renders the component', () => {
-    const { container } = render(<Tabs />);
+    const { container } = render(
+      <Tabs>
+        <Tabs.Panel key="FoR" title="Founding of Rome">
+          Arma virumque cano, Troiae qui primus ab oris.
+        </Tabs.Panel>
+        <Tabs.Panel key="MaR" title="Monarchy and Republic">
+          Senatus Populusque Romanus.
+        </Tabs.Panel>
+        <Tabs.Panel key="Emp" title="Empire">
+          Alea jacta est.
+        </Tabs.Panel>
+      </Tabs>,
+    );
     expect(container).toBeInTheDocument();
   });
   it('renders the style correctly', () => {
-    const style = styles();
+    const { base, tabList } = styles({ look: 'default', orientation: 'horizontal' });
     // TODO: use some variants for test
-    expect(style).toBe('');
+    expect(base()).toBe('flex flex-col');
+    expect(tabList()).toBe('relative z-10 flex mb-[-1px] flex-row gap-1');
   });
 });
