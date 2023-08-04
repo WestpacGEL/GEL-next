c266707107ce99536fe281fac9b08f953f603d49
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-08-04 15:02:49 +1000

    fix

--- apps/docs/src/components/sidebar-select/components/popover/popover.types.ts
+++ apps/docs/src/components/sidebar-select/components/popover/popover.types.ts
@@ -1,14 +1,10 @@
 import { HTMLAttributes, ReactNode } from 'react';
 import { type AriaPopoverProps } from 'react-aria';
 import { type OverlayTriggerState } from 'react-stately';
-import { type VariantProps } from 'tailwind-variants';
-
-import { styles } from './popover.styles.js';
 
 export type PopoverProps = Omit<AriaPopoverProps, 'popoverRef'> & {
   children: ReactNode;
   className?: string;
   popoverRef?: React.RefObject<HTMLDivElement>;
   state: OverlayTriggerState;
-} & VariantProps<typeof styles> &
-  HTMLAttributes<Element>;
+} & HTMLAttributes<Element>;
