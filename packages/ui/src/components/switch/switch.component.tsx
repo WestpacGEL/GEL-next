5b0fa326cd4bd4ce61a6f8257e5db461e09bf248
Tom King <tom.king@westpac.com.au>
2023-08-18 08:45:24 +1000

    - adding useId for aria-labelledby

--- packages/ui/src/components/switch/switch.component.tsx
+++ packages/ui/src/components/switch/switch.component.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useId, useRef } from 'react';
 import { VisuallyHidden, useCheckbox, useFocusRing } from 'react-aria';
 import { useToggleState } from 'react-stately';
 
@@ -7,15 +7,16 @@ import { type SwitchProps } from './switch.types.js';
 
 export function Switch({ className, label, size = 'medium', block = false, isDisabled, ...props }: SwitchProps) {
   const state = useToggleState(props);
+  const labelId = useId();
   const ref = useRef(null);
-  const { inputProps } = useCheckbox({ isDisabled, 'aria-labelledby': 'label', ...props }, state, ref);
+  const { inputProps } = useCheckbox({ isDisabled, 'aria-labelledby': labelId, ...props }, state, ref);
   const { isSelected } = state;
   const { isFocusVisible, focusProps } = useFocusRing();
   const styles = switchStyles({ block, isFocusVisible, isSelected, isDisabled, size });
 
   return (
     <label className={styles.base({ className })}>
-      <span className={styles.label()} id="label">
+      <span className={styles.label()} id={labelId}>
         {label}
       </span>
       <VisuallyHidden>
