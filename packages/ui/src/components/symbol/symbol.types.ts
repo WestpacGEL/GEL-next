3ed5cf9fe388bf900e173ba5f129e3b6c573da88
Jeremy Ortiz <jeremy.ortiz@westpac.com.au>
2023-08-30 22:17:46 +1000

    chore(ui): update symbol type descriptions

--- packages/ui/src/components/symbol/symbol.types.ts
+++ packages/ui/src/components/symbol/symbol.types.ts
@@ -6,13 +6,28 @@ import { styles } from './symbol.styles.js';
 export type Align = 'left' | 'center' | 'right';
 
 export type SymbolProps = Omit<SVGAttributes<SVGElement>, 'offset'> & {
+  /**
+   * SVG alignment
+   */
   align?: Align;
+  /**
+   * Symbol copyright year
+   */
   copyrightYear?: string;
+  /**
+   * Symbol offset alignment array i.e. [left, center, right]
+   */
   offset?: Array<number | null>;
   /**
    * Tag to render
    */
   tag?: keyof JSX.IntrinsicElements;
+  /**
+   * SVG viewBox height
+   */
   viewBoxHeight?: number;
+  /**
+   * SVG viewBox width
+   */
   viewBoxWidth?: number;
 } & VariantProps<typeof styles>;
