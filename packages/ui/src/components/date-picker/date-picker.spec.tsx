777e7de2788deea5fcef6b2cc55d063675770569
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-08-31 15:19:14 +1000

    fix

--- packages/ui/src/components/date-picker/date-picker.spec.tsx
+++ packages/ui/src/components/date-picker/date-picker.spec.tsx
@@ -2,15 +2,22 @@ import { render } from '@testing-library/react';
 
 import { DatePicker } from './date-picker.component.js';
 import { styles } from './date-picker.styles.js';
+import { createDate, formatDate } from './date-picker.utils.js';
 
 describe('DatePicker', () => {
   it('renders the component', () => {
     const { container } = render(<DatePicker />);
     expect(container).toBeInTheDocument();
   });
+
   it('renders the style correctly', () => {
-    const style = styles();
+    const style = styles({ size: 'lg' });
     // TODO: use some variants for test
-    expect(style).toBe('');
+    expect(style).toBe('date-picker date-picker-lg');
+  });
+  describe('DatePicker utils', () => {
+    it('should format the date', () => {
+      expect(formatDate(createDate('2023', '08', '01') ?? new Date(), 'DD/MM/YYYY')).toBe('01/08/2023');
+    });
   });
 });
