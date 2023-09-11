b3bcdce956918e127f11a6d632f2c41b2f63e3ea
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-09-11 18:01:19 +1000

    fix

--- packages/ui/src/components/modal/modal.spec.tsx
+++ packages/ui/src/components/modal/modal.spec.tsx
@@ -1,10 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, renderHook, screen } from '@testing-library/react';
+import { useOverlayTriggerState } from 'react-stately';
 
 import { Modal } from './modal.component.js';
 
 describe('Modal', () => {
   it('renders the component', () => {
-    const { container } = render(<Modal />);
-    expect(container).toBeInTheDocument();
+    const { result } = renderHook(() => useOverlayTriggerState({ isOpen: true }));
+
+    render(<Modal state={result.current}>Content</Modal>);
+
+    expect(screen.getByText('Content')).toBeInTheDocument();
+  });
+  it('not shows when it is hidden', () => {
+    const { result } = renderHook(() => useOverlayTriggerState({ isOpen: false }));
+
+    render(<Modal state={result.current}>Content</Modal>);
+
+    expect(screen.queryByText('Content')).not.toBeInTheDocument();
   });
 });
