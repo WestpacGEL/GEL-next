6e3c88a67b1fa67246f0973c943b079da0a80b2d
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-09-18 17:12:31 +1000

    update test

--- packages/ui/src/components/input-field/input-field.spec.tsx
+++ packages/ui/src/components/input-field/input-field.spec.tsx
@@ -1,11 +1,17 @@
 import { render } from '@testing-library/react';
 
+import { Button, Input } from '../index.js';
+
 import { InputField } from './input-field.component.js';
 import { styles } from './input-field.styles.js';
 
 describe('InputField', () => {
   it('renders the component', () => {
-    const { container } = render(<InputField />);
+    const { container } = render(
+      <InputField label="Label" hint="I am a hint" supportingText="I am supporting text" after={<Button>Check</Button>}>
+        <Input />
+      </InputField>,
+    );
     expect(container).toBeInTheDocument();
   });
   it('renders the style correctly', () => {
