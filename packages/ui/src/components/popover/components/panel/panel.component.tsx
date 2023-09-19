ee13e1b60bb1af96251346f3103b6af207ffe82b
Tom King <tom.jack.king@gmail.com>
2023-09-20 09:52:47 +1000

    - functionality working same as current GEL, needs accessibility

--- packages/ui/src/components/popover/components/panel/panel.component.tsx
+++ packages/ui/src/components/popover/components/panel/panel.component.tsx
@@ -1,14 +1,5 @@
 import React, { useRef } from 'react';
-import {
-  DismissButton,
-  Overlay,
-  mergeProps,
-  useFocusManager,
-  useOverlay,
-  useOverlayPosition,
-  usePopover,
-  usePreventScroll,
-} from 'react-aria';
+import { Overlay, mergeProps, useOverlay, useOverlayPosition, usePreventScroll } from 'react-aria';
 
 import { Button } from '../../../button/index.js';
 import { CloseIcon } from '../../../icon/index.js';
@@ -18,16 +9,11 @@ import { type PanelProps } from './panel.types.js';
 
 /**
  * TODO: Match functionality with current GEL
- * Missing: Can't type in text boxes outside of panel while open
- * Missing: Can't select text outisde of panel while open
- * Missing: Can't tab select out of panel while open
  */
 export function Panel({ children, state, placement = 'top', offset = 15, id, ...props }: PanelProps) {
   const popoverRef = useRef(null);
 
-  // const { overlayProps } = useOverlay({ isDismissable: false }, popoverRef);
-
-  const { overlayProps, underlayProps } = useOverlay(
+  const { overlayProps } = useOverlay(
     {
       isOpen: state.isOpen,
       onClose: state.close,
@@ -44,24 +30,13 @@ export function Panel({ children, state, placement = 'top', offset = 15, id, ...
     targetRef: props.triggerRef,
     overlayRef: popoverRef,
     isOpen: state.isOpen,
-    onClose: undefined,
     shouldUpdatePosition: true,
+    onClose: () => null,
+    shouldFlip: false,
   });
 
   usePreventScroll({ isDisabled: true });
 
-  // const { popoverProps, arrowProps } = usePopover(
-  //   {
-  //     ...props,
-  //     offset,
-  //     popoverRef,
-  //     placement,
-  //     // isNonModal will make it so that the popover can't be dismissed by clicking outside of it
-  //     isNonModal: true,
-  //     shouldFlip: false,
-  //   },
-  //   state,
-  // );
   const styles = panelStyles({ placement });
 
   return (
