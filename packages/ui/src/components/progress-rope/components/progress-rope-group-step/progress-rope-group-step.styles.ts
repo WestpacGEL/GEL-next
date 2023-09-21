3757cfd331144f7042df3e9527a392d165788988
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-09-21 15:57:04 +1000

    fix(style): border fix and styles refactors

--- packages/ui/src/components/progress-rope/components/progress-rope-group-step/progress-rope-group-step.styles.ts
+++ packages/ui/src/components/progress-rope/components/progress-rope-group-step/progress-rope-group-step.styles.ts
@@ -6,7 +6,7 @@ export const styles = tv(
       base: '',
       circle: 'relative z-10 h-[14px] w-[14px] border-2 bg-white transition-colors',
       circleWrapper:
-        'typography-body-10 relative flex h-7 cursor-pointer items-center gap-2 transition-colors before:absolute before:inset-y-0 before:left-[6px] before:block before:-translate-y-1/2 before:border-l-2 before:border-dotted before:transition-colors after:absolute after:inset-y-0 after:left-[6px] after:block after:translate-y-1/2 after:border-l-2 after:transition-colors',
+        'typography-body-10 relative flex h-7 cursor-pointer items-center gap-2 transition-colors before:absolute before:inset-y-0 before:left-[6px] before:block before:-translate-y-1/2 before:border-l-2 before:border-dotted before:transition-colors after:absolute after:inset-y-0 after:left-[6px] after:z-[1] after:block after:translate-y-1/2 after:border-l-2 after:transition-colors',
       stepsWrapper: 'relative overflow-hidden transition-all',
     },
     variants: {
