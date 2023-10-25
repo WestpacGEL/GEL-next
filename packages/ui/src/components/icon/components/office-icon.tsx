dc6e41b72dc0820ba689a3515b7ee1589e23933c
Jeremy Ortiz <jeremy.ortiz@westpac.com.au>
2023-10-26 09:07:37 +1100

    feature(icons): update and some new icons, remove brand logo icons

--- packages/ui/src/components/icon/components/office-icon.tsx
+++ packages/ui/src/components/icon/components/office-icon.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { Icon } from '../icon.component.js';
 import { type IconProps } from '../icon.types.js';
 
-export function OfficeIcon({ 'aria-label': ariaLabel = 'Office', copyrightYear = '2023', ...props }: IconProps) {
+export function OfficeIcon({
+  look = 'filled',
+  'aria-label': ariaLabel = 'Office',
+  copyrightYear = '2023',
+  ...props
+}: IconProps) {
   return (
     <Icon aria-label={ariaLabel} copyrightYear={copyrightYear} {...props}>
-      <path d="M20 10H16V12H20V10Z" fill="currentColor" />
-      <path d="M16 14H20V16H16V14Z" fill="currentColor" />
-      <path d="M20 18H16V20H20V18Z" fill="currentColor" />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M14 6H24V24H0V0H14V6ZM14 8H22V22H14V8ZM2 4H6V8H2V4ZM12 4H8V8H12V4ZM2 10H6V14H2V10ZM12 10H8V14H12V10ZM2 16H6V20H2V16ZM12 16H8V20H12V16Z"
-        fill="currentColor"
-      />
+      {look === 'filled' ? (
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M14 0L0 4V24H24V6H14V0ZM4 10H10V12H4V10ZM4 14H10V16H4V14ZM10 18H4V20H10V18ZM16 10H20V12H16V10ZM16 14H20V16H16V14ZM20 18H16V20H20V18Z"
+          fill="currentColor"
+        />
+      ) : (
+        <Fragment>
+          <path d="M10 10H4V12H10V10Z" fill="currentColor" />
+          <path d="M10 14H4V16H10V14Z" fill="currentColor" />
+          <path d="M4 18H10V20H4V18Z" fill="currentColor" />
+          <path d="M20 10H16V12H20V10Z" fill="currentColor" />
+          <path d="M16 14H20V16H16V14Z" fill="currentColor" />
+          <path d="M20 18H16V20H20V18Z" fill="currentColor" />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M14 6V0L0 4V24H24V6H14ZM2 22V5.5086L12 2.65146V22H2ZM14 22V8H22V22H14Z"
+            fill="currentColor"
+          />
+        </Fragment>
+      )}
     </Icon>
   );
 }
---
+++ packages/ui/src/components/icon/components/tiles-icon.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react';
+
+import { Icon } from '../icon.component.js';
+import { type IconProps } from '../icon.types.js';
+
+export function TilesIcon({
+  look = 'filled',
+  'aria-label': ariaLabel = 'Tiles',
+  copyrightYear = '2023',
+  ...props
+}: IconProps) {
+  return (
+    <Icon aria-label={ariaLabel} copyrightYear={copyrightYear} {...props}>
+      {look === 'filled' ? (
+        <Fragment>
+          <path
+            d="M4 2C2.89543 2 2 2.89543 2 4V9C2 10.1046 2.89543 11 4 11H9C10.1046 11 11 10.1046 11 9V4C11 2.89543 10.1046 2 9 2H4Z"
+            fill="currentColor"
+          />
+          <path
+            d="M4 13C2.89543 13 2 13.8954 2 15V20C2 21.1046 2.89543 22 4 22H9C10.1046 22 11 21.1046 11 20V15C11 13.8954 10.1046 13 9 13H4Z"
+            fill="currentColor"
+          />
+          <path
+            d="M13 4C13 2.89543 13.8954 2 15 2H20C21.1046 2 22 2.89543 22 4V9C22 10.1046 21.1046 11 20 11H15C13.8954 11 13 10.1046 13 9V4Z"
+            fill="currentColor"
+          />
+          <path
+            d="M15 13C13.8954 13 13 13.8954 13 15V20C13 21.1046 13.8954 22 15 22H20C21.1046 22 22 21.1046 22 20V15C22 13.8954 21.1046 13 20 13H15Z"
+            fill="currentColor"
+          />
+        </Fragment>
+      ) : (
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M11 20V15C11 13.8954 10.1046 13 9 13H4C2.89543 13 2 13.8954 2 15V20C2 21.1046 2.89543 22 4 22H9C10.1046 22 11 21.1046 11 20ZM4 4V9H9V4H4ZM4 15V20H9V15H4ZM20 4H15V9H20V4ZM15 15V20H20V15H15ZM2 4C2 2.89543 2.89543 2 4 2H9C10.1046 2 11 2.89543 11 4V9C11 10.1046 10.1046 11 9 11H4C2.89543 11 2 10.1046 2 9V4ZM13 4C13 2.89543 13.8954 2 15 2H20C21.1046 2 22 2.89543 22 4V9C22 10.1046 21.1046 11 20 11H15C13.8954 11 13 10.1046 13 9V4ZM13 15C13 13.8954 13.8954 13 15 13H20C21.1046 13 22 13.8954 22 15V20C22 21.1046 21.1046 22 20 22H15C13.8954 22 13 21.1046 13 20V15Z"
+          fill="currentColor"
+        />
+      )}
+    </Icon>
+  );
+}
