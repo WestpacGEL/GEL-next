3984faf191831f1303d7854a70acc7a90f5c085c
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-10-26 15:41:29 +1000

    check if the process.cwd

--- apps/site/src/app/reader.ts
+++ apps/site/src/app/reader.ts
@@ -2,4 +2,5 @@ import { createReader } from '@keystatic/core/reader';
 
 import keystaticConfig from '../../keystatic.config';
 
+console.log('process.cwd()', process.cwd());
 export const reader = createReader(process.cwd(), keystaticConfig);
