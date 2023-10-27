4bc0afde57e8730b77538521a9ce97a40f6e81d1
Tom King <tom.jack.king@gmail.com>
2023-10-27 10:19:06 +1000

    - adjusted size of border radius on components that needed it

--- packages/ui/src/components/button-group/components/button/button.styles.ts
+++ packages/ui/src/components/button-group/components/button/button.styles.ts
@@ -5,7 +5,7 @@ export const styles = tv(
     slots: {
       base: '',
       button:
-        'rounded-none hover:cursor-pointer group-first/buttons:rounded-l group-last/buttons:rounded-r group-[:not(:first-child)]/buttons:border-l-0 active-theme-rams:border-b-primary active-theme-rams:before:hidden',
+        'rounded-none hover:cursor-pointer group-first/buttons:rounded-l-[0.1875rem] group-last/buttons:rounded-r-[0.1875rem] group-[:not(:first-child)]/buttons:border-l-0 active-theme-rams:border-b-primary active-theme-rams:before:hidden',
     },
     variants: {
       isDisabled: {
