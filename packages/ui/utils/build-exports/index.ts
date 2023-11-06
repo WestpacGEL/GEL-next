d9eb910db5f67e97db1a615d9df5745564850d7b
Jeremy Ortiz <jeremy.ortiz@westpac.com.au>
2023-11-06 11:50:52 +1100

    feature(ui): add script to generate individual component exports

---
+++ packages/ui/utils/build-exports/index.ts
@@ -0,0 +1,38 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const exports: Record<string, { default: string }> = {
+  '.': {
+    default: './dist/index.js',
+  },
+  './component-type.json': {
+    default: './dist/component-type.json',
+  },
+  './css': {
+    default: './dist/css/westpac-ui.min.css',
+  },
+  './hook': {
+    default: './dist/hook/index.js',
+  },
+  './tailwind': {
+    default: './dist/tailwind/index.js',
+  },
+};
+
+const main = async () => {
+  const components = (await fs.readdir(path.join(process.cwd(), 'src/components'), { withFileTypes: true }))
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+  components.forEach(component => {
+    exports[`./${component}`] = { default: `./dist/components/${component}/index.js` };
+  });
+
+  const pkgJsonPath = path.join(process.cwd(), 'package.json');
+  const data = await fs.readFile(pkgJsonPath, 'utf-8');
+  const pkgJson = JSON.parse(data);
+  pkgJson.exports = exports;
+  await fs.writeFile(pkgJsonPath, JSON.stringify(pkgJson));
+};
+
+main();
