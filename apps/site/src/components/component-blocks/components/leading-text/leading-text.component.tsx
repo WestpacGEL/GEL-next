f8c9f4f91c6f3e60af2ce6dbcb067f02a9e6bdab
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-11-14 15:33:24 +1000

    fix

--- apps/site/src/components/component-blocks/components/leading-text/leading-text.component.tsx
+++ apps/site/src/components/component-blocks/components/leading-text/leading-text.component.tsx
@@ -2,6 +2,5 @@ import { styles } from './leading-text.style';
 import { type HeroImageProps } from './leading-text.types';
 
 export const LeadingText = ({ text }: HeroImageProps) => {
-  console.log('text', text);
   return <blockquote className={styles({})}>{text}</blockquote>;
 };
