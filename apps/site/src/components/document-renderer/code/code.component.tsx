7620de10e7659fae3d9dd8b274488be0f2897d9d
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-11-23 09:40:16 +1000

    fixes #338 default open

--- apps/site/src/components/document-renderer/code/code.component.tsx
+++ apps/site/src/components/document-renderer/code/code.component.tsx
@@ -4,9 +4,9 @@ import { Code as CodeComponent } from '../../code/code.component';
 
 import { type CodeProps } from './code.types';
 
-export const Code = ({ children, ...props }: CodeProps) => {
+export const Code = ({ children, showCode, ...props }: CodeProps) => {
   return (
-    <CodeComponent className="my-3" live {...props}>
+    <CodeComponent className="my-3" showCode={showCode} live {...props}>
       {children as ReactNode}
     </CodeComponent>
   );
