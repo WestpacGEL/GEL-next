9686eed36b39c9a4f0ff7a010e038400d8c37b95
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-11-22 12:40:13 +1000

    fixes #314

--- apps/site/src/app/design-system/[...component]/components/header/header.styles.ts
+++ apps/site/src/app/design-system/[...component]/components/header/header.styles.ts
@@ -4,7 +4,7 @@ export const styles = tv(
   {
     slots: {
       base: 'sticky top-0 z-50 flex w-full items-center gap-2 bg-hero px-2 py-3.5 transition-colors sm:px-4 md:top-[-10.125rem] md:h-[14.25rem] md:items-end',
-      gridButton: 'flex h-6 gap-[0.125rem] p-1',
+      gridButton: 'flex h-6 items-stretch gap-[0.125rem] p-1',
       gridButtonWrapper: 'fixed right-2 top-2 hidden items-center text-white sm:flex',
       hamburgerButton: 'fixed left-0 top-0 block h-4 w-4 translate-y-[-0.125rem] px-2 py-3.5 sm:left-2 lg:hidden',
       title: 'typography-brand-7 pl-6 font-normal leading-none text-white sm:pl-6 lg:pl-0',
@@ -17,7 +17,7 @@ export const styles = tv(
         },
         false: {
           base: '',
-          title: 'md:typography-brand-2 md:px-3 md:py-5',
+          title: 'md:typography-brand-2 md:px-2 md:pb-[1.75rem] md:pt-5 md:leading-none lg:ml-2',
         },
       },
       brand: {
