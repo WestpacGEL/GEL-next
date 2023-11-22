a2bee2bf230d3f49c5c435d76c0336e572765234
Kenji Shiroma <kenjishiromajp@gmail.com>
2023-11-22 14:53:47 +1000

    fixes #328 margin for phone

--- apps/site/src/components/component-blocks/components/leading-text/leading-text.style.ts
+++ apps/site/src/components/component-blocks/components/leading-text/leading-text.style.ts
@@ -1,5 +1,5 @@
 import { tv } from 'tailwind-variants';
 
 export const styles = tv({
-  base: 'typography-site-8 mx-0 mb-9 leading-[1.6] xsl:typography-site-7 xsl:leading-[1.6] md:-mx-17',
+  base: 'typography-site-8 mx-0 mb-7 leading-[1.6] xsl:typography-site-7 xsl:mb-9 xsl:leading-[1.6] md:-mx-17',
 });
