98cbd3e82378e7791692d087a73b4e94884b04d3
Tom King <tom.jack.king@gmail.com>
2023-11-24 16:00:23 +1000

    - fixed visual bug where Home menu option wasn't highlighting

--- apps/site/src/app/design-system/components/sidebar/components/navigation/navigation.component.tsx
+++ apps/site/src/app/design-system/components/sidebar/components/navigation/navigation.component.tsx
@@ -82,7 +82,9 @@ function Group({ label, level, crumbs, children, ...props }: GroupProps) {
 function Item({ label, path, level, crumbs, brand, ...props }: ItemProps) {
   const href = `/design-system/${path}?brand=${brand}`;
   const page = path?.split('/').pop();
-  const active = crumbs[crumbs.length - 1] === page;
+  const active = crumbs[crumbs.length - 1] === page || (crumbs[crumbs.length - 1] === 'design-system' && page === '');
+  console.log(page);
+  console.log(crumbs[crumbs.length - 1]);
   const { setOpen } = useSidebar();
 
   return (
