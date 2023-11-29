98a9593bb9f64f5a82d942c13aaaed9827196dfe
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2023-11-29 13:56:35 +1000

    fixes #410 block responsive

--- packages/ui/src/components/button/button.styles.ts
+++ packages/ui/src/components/button/button.styles.ts
@@ -6,7 +6,7 @@ import { tv } from 'tailwind-variants';
 export const styles = tv(
   {
     slots: {
-      base: 'inline-flex items-center justify-center rounded leading-[1.5] transition-[background] disabled:pointer-events-none disabled:opacity-50 group-[.add-on-after]:rounded-l-none group-[.add-on-before]:rounded-r-none',
+      base: 'items-center justify-center rounded leading-[1.5] transition-[background] disabled:pointer-events-none disabled:opacity-50 group-[.add-on-after]:rounded-l-none group-[.add-on-before]:rounded-r-none',
       iconBefore: '',
       iconAfter: '',
       dropdown: 'ml-[0.4em]',
@@ -14,17 +14,17 @@ export const styles = tv(
     },
     variants: {
       size: {
-        small: { base: 'typography-body-10 px-2 pb-[0.25rem] pt-0.5 active-theme-rams:before:h-[0.0625rem]' },
-        medium: { base: 'typography-body-9 px-2 py-[0.3125rem] active-theme-rams:before:h-[0.125rem]' },
-        large: { base: 'typography-body-9 px-2.5 py-[0.5rem] active-theme-rams:before:h-0.5' },
-        xlarge: { base: 'typography-body-8 px-3 pb-[0.625rem] pt-1.5 active-theme-rams:before:h-[0.25rem]' },
+        small: { base: 'typography-body-10 active-theme-rams:before:h-[0.0625rem] px-2 pb-[0.25rem] pt-0.5' },
+        medium: { base: 'typography-body-9 active-theme-rams:before:h-[0.125rem] px-2 py-[0.3125rem]' },
+        large: { base: 'typography-body-9 active-theme-rams:before:h-0.5 px-2.5 py-[0.5rem]' },
+        xlarge: { base: 'typography-body-8 active-theme-rams:before:h-[0.25rem] px-3 pb-[0.625rem] pt-1.5' },
       },
       look: {
         primary: {
-          base: 'relative border border-primary bg-primary text-white hover:bg-primary-70 active:bg-primary-50 active-theme-rams:border-b-pop active-theme-rams:before:absolute active-theme-rams:before:bottom-0 active-theme-rams:before:block active-theme-rams:before:w-full active-theme-rams:before:bg-pop',
+          base: 'border-primary bg-primary hover:bg-primary-70 active:bg-primary-50 active-theme-rams:border-b-pop active-theme-rams:before:absolute active-theme-rams:before:bottom-0 active-theme-rams:before:block active-theme-rams:before:w-full active-theme-rams:before:bg-pop relative border text-white',
         },
-        hero: { base: 'border border-hero bg-hero text-white hover:bg-hero-70 active:bg-hero-50' },
-        faint: { base: 'border border-border bg-light text-muted hover:bg-white active:bg-white' },
+        hero: { base: 'border-hero bg-hero hover:bg-hero-70 active:bg-hero-50 border text-white' },
+        faint: { base: 'border-border bg-light text-muted border hover:bg-white active:bg-white' },
         link: { base: 'text-link underline' },
       },
       soft: {
@@ -32,7 +32,7 @@ export const styles = tv(
       },
       block: {
         true: { base: 'flex w-full' },
-        false: { base: '' },
+        false: { base: 'inline-flex w-auto' },
       },
       justify: {
         true: { base: 'justify-between' },
@@ -50,7 +50,7 @@ export const styles = tv(
         slots: ['base'],
         look: 'primary',
         soft: true,
-        className: 'text-text hover:text-white active-theme-rams:text-primary active-theme-rams:hover:text-white',
+        className: 'text-text active-theme-rams:text-primary active-theme-rams:hover:text-white hover:text-white',
       },
       {
         slots: ['base'],
