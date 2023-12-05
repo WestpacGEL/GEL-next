62e2397290d1453ca5fae7534b6cdda68d6223e9
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2023-12-05 16:44:32 +1000

    fix

--- apps/site/src/components/code/components/demos/tax-file-number/tax-file-number.demo.tsx
+++ apps/site/src/components/code/components/demos/tax-file-number/tax-file-number.demo.tsx
@@ -62,7 +62,7 @@ export const TaxFileNumberPattern = ({ showPrevious = false, showErrors = false 
       {value === 'provide-later' && (
         <Alert>
           {/* TODO: BRand.Name */}
-          You can provide your TFN at any time via phone or at a "BRAND.name" branch. In the meantime, please note, we
+          You can provide your TFN at any time via phone or at a BRAND.name branch. In the meantime, please note, we
           may need to withhold tax from any interest you earn.
         </Alert>
       )}
