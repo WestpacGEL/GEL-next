b110cc884a720a98041444e100882889900bc898
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2023-12-06 14:49:09 +1000

    fix

--- apps/site/src/components/code/components/live-code/live-code.styles.ts
+++ apps/site/src/components/code/components/live-code/live-code.styles.ts
@@ -6,7 +6,8 @@ export const styles = tv({
     displayWrapper: 'relative -mx-6 -mt-6 border-muted-50 p-6',
     error: 'flex gap-2 rounded-md bg-danger-10 p-2 text-danger-90',
     buttonWrapper: '-mx-6 -mb-6 flex items-center justify-end ',
-    codeWrapper: 'relative -mx-6 border-t border-muted-50 bg-[#282c34] p-[0.875rem] font-monospace text-base',
+    codeWrapper:
+      'relative -mx-6 border-t border-muted-50 bg-[#282c34] p-[0.875rem] font-monospace text-base leading-loose',
     arrowIcon: 'transition-transform',
     copyCodeButton:
       'typography-body-10 absolute right-0 top-0 p-1 pr-2 text-white opacity-50 transition-opacity hover:opacity-100',
