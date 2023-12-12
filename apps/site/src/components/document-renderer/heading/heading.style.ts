9f67947511b8f031d527130c24a592cbf992a69d
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2023-12-12 10:29:21 +1000

    fixes #530 h4 with uppercase

--- apps/site/src/components/document-renderer/heading/heading.style.ts
+++ apps/site/src/components/document-renderer/heading/heading.style.ts
@@ -12,7 +12,7 @@ export const styles = tv({
       1: 'typography-body-5 mb-3',
       2: 'typography-body-7 mb-4 sm:typography-body-6 sm:mb-7',
       3: 'typography-body-8 mb-2',
-      4: 'typography-body-9 mb-2',
+      4: 'typography-body-9 mb-2 uppercase',
       5: 'typography-body-10 mb-2',
       6: 'typography-body-10 mb-2',
     },
