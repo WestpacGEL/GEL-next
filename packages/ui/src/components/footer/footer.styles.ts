e25edc57ba4a7d45aa88a077cb316d0d36053bec
Tom King <tom.jack.king@gmail.com>
2023-12-22 09:18:58 +1000

    - fixed border thickness

--- packages/ui/src/components/footer/footer.styles.ts
+++ packages/ui/src/components/footer/footer.styles.ts
@@ -3,7 +3,7 @@ import { tv } from 'tailwind-variants';
 export const styles = tv(
   {
     slots: {
-      base: 'relative overflow-hidden border-t-2 border-t-primary',
+      base: 'relative overflow-hidden border-t-[1px] border-t-primary',
       wrapper: 'pt-3 max-md:px-2 max-md:pb-3 md:px-4 md:pb-4',
       topRow: 'max-md:mb-7 md:mb-4',
       link: 'float-right block',
