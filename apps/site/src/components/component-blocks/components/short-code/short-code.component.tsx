68f8fbee6833f5e2d4716936ff2298088b8ce3a8
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2024-01-11 14:20:39 +1000

    fix

--- apps/site/src/components/component-blocks/components/short-code/short-code.component.tsx
+++ apps/site/src/components/component-blocks/components/short-code/short-code.component.tsx
@@ -3,18 +3,12 @@ import { useMemo } from 'react';
 
 import { DOCUMENT_RENDERERS } from '@/app/design-system/[...component]/components/content-tabs/components/document-renderer';
 
-import { ArticleComponentBlocksComponents } from '../../article-component-blocks';
-
 import { type ShortCodeProps } from './short-code.types';
 
 export const ShortCode = ({ name, shortCodes }: ShortCodeProps) => {
   const shortCode = useMemo(() => shortCodes.find(shortCode => shortCode.name === name), [name, shortCodes]);
 
   return shortCode?.content ? (
-    <DocumentRenderer
-      document={shortCode.content}
-      renderers={DOCUMENT_RENDERERS}
-      componentBlocks={{}}
-    />
+    <DocumentRenderer document={shortCode.content} renderers={DOCUMENT_RENDERERS} componentBlocks={{}} />
   ) : null;
 };
