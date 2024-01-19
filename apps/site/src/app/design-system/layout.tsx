5da2c3c859f799ddfe6e3b4b93ec2c3e1b29625c
samithaf <fernando.samitha@gmail.com>
2024-01-19 16:13:55 +1100

    added a log to debug content

--- apps/site/src/app/design-system/layout.tsx
+++ apps/site/src/app/design-system/layout.tsx
@@ -6,6 +6,8 @@ import { Sidebar, SidebarContextProvider } from './components';
 
 export default async function DesignSystemLayout({ children }: { children: React.ReactNode }) {
   const allContent = await reader().collections.designSystem.all();
+  // eslint-disable-next-line no-console
+  console.log('allContent', JSON.stringify(allContent));
   const formattedItems = sortMenu(formatNavItems(allContent.map(({ entry, slug }) => ({ slug, name: entry.name }))));
 
   return (
