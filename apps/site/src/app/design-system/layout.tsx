e5c2d3d47bd5e94400f394c315cfca2a2ecf0d22
samithaf <fernando.samitha@gmail.com>
2024-01-22 09:47:14 +1100

    removed generateStaticParams fn from DesignSystemLayout

--- apps/site/src/app/design-system/layout.tsx
+++ apps/site/src/app/design-system/layout.tsx
@@ -4,11 +4,6 @@ import { formatNavItems, sortMenu } from '@/utils/format';
 
 import { Sidebar, SidebarContextProvider } from './components';
 
-export async function generateStaticParams() {
-  await reader().collections.designSystem.all();
-  return [];
-}
-
 export default async function DesignSystemLayout({ children }: { children: React.ReactNode }) {
   const allContent = await reader().collections.designSystem.all();
   const formattedItems = sortMenu(formatNavItems(allContent.map(({ entry, slug }) => ({ slug, name: entry.name }))));
