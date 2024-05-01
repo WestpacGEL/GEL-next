734a3e7a47c5e8bd2915f1ab87a6bb11c8d16430
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2024-05-01 12:47:19 +1000

    fix

--- packages/ui/src/components/grid/components/grid-container/grid-container.styles.ts
+++ packages/ui/src/components/grid/components/grid-container/grid-container.styles.ts
@@ -5,8 +5,8 @@ export const styles = tv(
     base: 'mx-auto box-border w-full',
     variants: {
       fixed: {
-        true: 'px-3 xsl:max-w-xsl xsl:px-5 sm:max-w-sm sm:px-6 md:max-w-md md:px-7 lg:max-w-lg lg:px-10 xl:px-0',
-        false: 'max-w-container px-4 xsl:px-5 sm:px-6 md:px-8 lg:px-10',
+        true: 'max-w-container px-3 xsl:max-w-xsl xsl:px-5 sm:max-w-sm sm:px-6 md:max-w-md md:px-7 lg:max-w-lg lg:px-10 xl:px-0',
+        false: 'px-4 xsl:px-5 sm:px-6 md:px-8 lg:px-10',
       },
     },
   },
