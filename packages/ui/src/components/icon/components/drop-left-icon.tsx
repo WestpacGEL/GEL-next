535285325c74768fb2e37d5cb2454c1123152bf8
Hannah <hannah.zhang1@westpac.com.au>
2024-05-21 11:04:49 +1000

    fixed aria label

--- packages/ui/src/components/icon/components/drop-left-icon.tsx
+++ packages/ui/src/components/icon/components/drop-left-icon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Icon } from '../icon.component.js';
 import { type IconProps } from '../icon.types.js';
 
-export function DropLeftIcon({ 'aria-label': ariaLabel = 'Drop Up', copyrightYear = '2024', ...props }: IconProps) {
+export function DropLeftIcon({ 'aria-label': ariaLabel = 'Drop Left', copyrightYear = '2024', ...props }: IconProps) {
   return (
     <Icon aria-label={ariaLabel} copyrightYear={copyrightYear} {...props}>
       <path d="m8 19 8-7-8-7v14Z" fill="currentColor" />
