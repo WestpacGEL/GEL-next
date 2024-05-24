a911ba3d5ce21fb199e5eadea4333bc7061d291a
Hannah <hannah.zhang1@westpac.com.au>
2024-05-24 12:16:57 +1000

    fixed code format

--- packages/ui/src/components/icon/components/drop-left-icon.tsx
+++ packages/ui/src/components/icon/components/drop-left-icon.tsx
@@ -6,7 +6,7 @@ import { type IconProps } from '../icon.types.js';
 export function DropLeftIcon({ 'aria-label': ariaLabel = 'Drop Left', copyrightYear = '2024', ...props }: IconProps) {
   return (
     <Icon aria-label={ariaLabel} copyrightYear={copyrightYear} {...props}>
-      <path d="m8 19 8-7-8-7v14Z" fill="currentColor"/>
+      <path d="m8 19 8-7-8-7v14Z" fill="currentColor" />
     </Icon>
   );
 }
