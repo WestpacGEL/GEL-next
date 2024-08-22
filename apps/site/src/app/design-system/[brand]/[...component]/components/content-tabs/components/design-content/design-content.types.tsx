c3a0ed5cf0319bd546b2085ca050469847530196
Hannah <hannah.zhang1@westpac.com.au>
2024-08-22 16:51:35 +1000

    fix: lint

--- apps/site/src/app/design-system/[brand]/[...component]/components/content-tabs/components/design-content/design-content.types.tsx
+++ apps/site/src/app/design-system/[brand]/[...component]/components/content-tabs/components/design-content/design-content.types.tsx
@@ -3,7 +3,7 @@ import { DocumentElement } from '@keystatic/core';
 import { RelatedInfoProps } from '@/components/related-info/related-info.types';
 import { ShortCode } from '@/types/short-code.types';
 
-export type DesignSectionProps = { content: DocumentElement[]; noTitle?: boolean; title: string; noDemo?: boolean };
+export type DesignSectionProps = { content: DocumentElement[]; noDemo?: boolean; noTitle?: boolean; title: string };
 
 export type DesignContentProps = {
   description?: string;
