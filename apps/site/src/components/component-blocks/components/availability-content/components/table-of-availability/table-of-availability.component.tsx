b7ccc4cb3e944da02323125545cff290c5f4ee27
Hannah <hannah.zhang1@westpac.com.au>
2024-08-30 10:07:02 +1000

    fix: change cell width from fix value to percentage

--- apps/site/src/components/component-blocks/components/availability-content/components/table-of-availability/table-of-availability.component.tsx
+++ apps/site/src/components/component-blocks/components/availability-content/components/table-of-availability/table-of-availability.component.tsx
@@ -41,7 +41,7 @@ export function TableOfAvailability({
   const styles = TableOfAvailabilityStyles({});
 
   const hasAlternativeNames = platforms.some(platform => platform.alternative);
-  const cellWidth = 200;
+  const cellWidth = '10%';
 
   return (
     <Table>
