3e6318911a74dc4bc85d857cfaa2578a0c3c2aad
HZ991 <hzha991@aucklanduni.ac.nz>
2024-10-31 14:35:19 +1100

    fix: header className mismatch bug for site

--- apps/site/src/app/design-system/[brand]/[...component]/components/header/header.component.tsx
+++ apps/site/src/app/design-system/[brand]/[...component]/components/header/header.component.tsx
@@ -11,7 +11,7 @@ import { styles as headerStyles } from './header.styles';
 const FIXED_HEADER = 162; // 228 - 66 = height to stick
 
 export function Header({ className, title, brand }: { brand: string; className?: string; title?: string }) {
-  const [fixed, setFixed] = useState(typeof window !== 'undefined' ? window.scrollY >= FIXED_HEADER : false);
+  const [fixed, setFixed] = useState(false);
   const styles = headerStyles({ brand: brand.toLowerCase() as BrandKey, fixed, className });
   const headerRef = useRef<HTMLHeadingElement>(null);
   const { setOpen } = useSidebar();
@@ -20,32 +20,21 @@ export function Header({ className, title, brand }: { brand: string; className?:
     if (typeof window !== 'undefined') {
       const handleScroll = () => {
         const isFixed = window.scrollY >= FIXED_HEADER;
+        console.log('window position:' + window.scrollY);
+        console.log('header position:' + FIXED_HEADER);
+        console.log('isFixed:' + isFixed);
         setFixed(isFixed);
       };
+      handleScroll();
       window.addEventListener('scroll', handleScroll);
+      window.addEventListener('load', handleScroll);
+
       return () => window.removeEventListener('scroll', handleScroll);
     }
   }, []);
 
   return (
     <header className={styles.base()}>
-      <div className={styles.gridButtonWrapper()}>
-        <span className="sr-only">Active breakpoint:</span>
-        <span className="hidden font-bold sm:block md:hidden">SM</span>
-        <span className="hidden font-bold md:block lg:hidden">MD</span>
-        <span className="hidden font-bold lg:block">LG</span>
-        <button
-          aria-hidden="true"
-          className={styles.gridButton()}
-          onClick={() => {
-            return;
-          }}
-        >
-          {new Array(4).fill(null).map((_, index) => {
-            return <span className="w-[0.25rem] items-stretch bg-white/30" key={index} />;
-          })}
-        </button>
-      </div>
       {/* The tab order on the original site was the grid button before wrapper when coming from the browser bar */}
       <button className={styles.hamburgerButton()} onClick={() => setOpen(open => !open)}>
         <HamburgerMenuIcon color="white" className="mx-auto" />
