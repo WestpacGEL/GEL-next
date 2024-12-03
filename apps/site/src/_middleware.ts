fbb25273bdda6620a7e4b21f6756301d27ff2e8b
Jeremy Ortiz <jeremy.ortiz@westpac.com.au>
2024-12-03 13:57:33 +1100

    chore: update img csp headers

--- apps/site/src/_middleware.ts
+++ apps/site/src/_middleware.ts
@@ -8,7 +8,7 @@ export function _middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: res.cloudinary.com westpac.com.au;
+    img-src 'self' blob: data: res.cloudinary.com www.westpac.com.au;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
