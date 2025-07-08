7c8dfbae2afdbbb20c7abab029f54c7378039096
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-07-09 08:29:38 +1000

    update lint config for ui

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -50,8 +50,8 @@ export default defineConfig([
     files: ['**/*.spec.tsx', '**/*.spec.ts'],
     rules: {
       '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'warn',
-      '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/unbound-method': 'off',
       'sonarjs/no-nested-functions': 'off',
     },
