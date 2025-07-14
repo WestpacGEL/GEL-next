f44f3fedfe26dc19e135633944e173daea842ba9
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-07-14 16:04:53 +1000

    fix pagination responsive

--- packages/ui/src/components/pagination/pagination.utils.ts
+++ packages/ui/src/components/pagination/pagination.utils.ts
@@ -1,10 +1,14 @@
-import { Breakpoint } from '../../tailwind/index.js';
+import { BREAKPOINTS, Breakpoint } from '../../tailwind/index.js';
 
+const BREAKPOINTS_DECRECENT = Object.keys(BREAKPOINTS).reverse();
 export function getSiblingOrBoundaryCount(
   count: Partial<Record<Breakpoint | 'initial', number>>,
   breakpoint: Breakpoint | 'initial',
 ) {
-  const finalBreakPoint = [breakpoint, 'initial', 'xsl', 'sm', 'md', 'lg', 'xl'].find(
+  const currentBreakpointIndex = Object.keys(BREAKPOINTS)
+    .reverse()
+    .findIndex(bp => bp === breakpoint);
+  const finalBreakPoint = [breakpoint, ...BREAKPOINTS_DECRECENT.slice(currentBreakpointIndex)].find(
     currentBreakpoint => count[currentBreakpoint as Breakpoint | 'initial'] !== undefined,
   ) as Breakpoint | 'initial';
   return count[finalBreakPoint] || 0;
