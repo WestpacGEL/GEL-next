f72fb0ccc899053eea640dd7e2080880a2afcfa0
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-07-14 16:13:58 +1000

    typo

--- packages/ui/src/components/pagination/pagination.utils.ts
+++ packages/ui/src/components/pagination/pagination.utils.ts
@@ -1,6 +1,6 @@
 import { BREAKPOINTS, Breakpoint } from '../../tailwind/constants/index.js';
 
-const BREAKPOINTS_DECRECENT = Object.keys(BREAKPOINTS).reverse();
+const BREAKPOINTS_DECRESCENT = Object.keys(BREAKPOINTS).reverse();
 export function getSiblingOrBoundaryCount(
   count: Partial<Record<Breakpoint | 'initial', number>>,
   breakpoint: Breakpoint | 'initial',
@@ -8,7 +8,7 @@ export function getSiblingOrBoundaryCount(
   const currentBreakpointIndex = Object.keys(BREAKPOINTS)
     .reverse()
     .findIndex(bp => bp === breakpoint);
-  const finalBreakPoint = [breakpoint, ...BREAKPOINTS_DECRECENT.slice(currentBreakpointIndex)].find(
+  const finalBreakPoint = [breakpoint, ...BREAKPOINTS_DECRESCENT.slice(currentBreakpointIndex)].find(
     currentBreakpoint => count[currentBreakpoint as Breakpoint | 'initial'] !== undefined,
   ) as Breakpoint | 'initial';
   return count[finalBreakPoint] || 0;
