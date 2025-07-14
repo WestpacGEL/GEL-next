314ec1d74da6b774b88615e6b6d8e8faef685497
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-07-14 16:32:50 +1000

    fix

--- packages/ui/src/components/date-picker/components/popover/popover.styles.ts
+++ packages/ui/src/components/date-picker/components/popover/popover.styles.ts
@@ -4,17 +4,17 @@ export const styles = tv(
   {
     slots: {
       underlay: 'fixed inset-0',
-      popover: 'absolute bg-white',
+      popover: 'bg-white',
     },
     variants: {
       showAsBottomSheet: {
         true: {
           underlay: 'animate-fadeIn bg-black/20',
-          popover: 'inset-x-0 !left-0 !top-auto bottom-0 animate-slideUp px-[8%] pb-3',
+          popover: 'animate-slideUp !fixed inset-x-0 !bottom-0 !left-0 !top-auto px-[8%] pb-3',
         },
         false: {
           underlay: '',
-          popover: 'scale-100 animate-fadeIn border border-border bg-white opacity-100 shadow-sm',
+          popover: 'absolute scale-100 animate-fadeIn border border-border bg-white opacity-100 shadow-sm',
         },
       },
     },
