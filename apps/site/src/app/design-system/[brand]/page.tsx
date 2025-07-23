f761889d5bb4a69a95a896386d47807c667a59e9
KateMacdonald <ktmacdonald@westpac.com.au>
2025-07-23 15:09:17 +1000

    Update page.tsx

--- apps/site/src/app/design-system/[brand]/page.tsx
+++ apps/site/src/app/design-system/[brand]/page.tsx
@@ -121,7 +121,7 @@ export default function DesignSystemHomePage({ params }: { params: { brand: stri
         <SectionHeading>Downloads &amp; links</SectionHeading>
         <Grid className="mb-5 sm:mb-9">
           <SectionItem className="flex flex-col gap-2">
-            <p className="leading-[2]">Design on-brand with greater efficiency and accuracy using the Sketch UI Kit.</p>
+            <p className="leading-[2]">Design on-brand with greater efficiency and accuracy using the Figma UI Kit.</p>
             <p className="leading-[2]">Visit GitHub to follow Westpac GEL Design System code updates and log issues.</p>
           </SectionItem>
         </Grid>
