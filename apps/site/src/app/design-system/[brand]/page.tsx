766c75033869b1aa896ff24690169b2236d71750
Tom King <tom.jack.king@gmail.com>
2025-08-13 10:39:15 +1000

    - fix pnpm lock - fix page

--- apps/site/src/app/design-system/[brand]/page.tsx
+++ apps/site/src/app/design-system/[brand]/page.tsx
@@ -2,6 +2,8 @@ import { Grid, GridItem } from '@westpac/ui';
 import { GithubIcon } from '@westpac/ui/icon';
 import Link from 'next/link';
 
+import { Hero, Section, SectionHeading, SectionItem } from '../components';
+
 import { BrandKey } from '@/app/types/brand.types';
 import {
   AtlassianLogo,
@@ -15,8 +17,6 @@ import {
   ShopifyLogo,
   TwitterLogo,
 } from '@/components/logos';
-
-import { Hero, Section, SectionHeading, SectionItem } from '../components';
 
 export default function DesignSystemHomePage({ params }: { params: { brand: string } }) {
   const brand = (params.brand || 'wbc') as BrandKey;
