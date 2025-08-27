31ee1f04c128c6c5437fcb4bf68d7d1c99f700b2
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-08-27 16:27:52 +1000

    fix token

--- apps/site/src/app/design-system/components/sidebar/sidebar.component.tsx
+++ apps/site/src/app/design-system/components/sidebar/sidebar.component.tsx
@@ -6,12 +6,12 @@ import { useParams, useRouter } from 'next/navigation';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useOnClickOutside } from 'usehooks-ts';
 
-import { CloseIcon } from '@/components/code/code.inject-components';
-import { BANK_OPTIONS } from '@/constants/bank-options';
-
 import { BackToGelSvg, Logo, Navigation, SidebarSelect } from './components';
 import { useSidebar } from './sidebar.context';
 import { SidebarProps } from './sidebar.types';
+
+import { CloseIcon } from '@/components/code/code.inject-components';
+import { BANK_OPTIONS } from '@/constants/bank-options';
 
 // Credits: https://github.com/jmarioste/next-responsive-sidebar-tailwind
 
@@ -67,7 +67,7 @@ export function Sidebar({ items, brand }: SidebarProps) {
     <>
       <div
         className={clsx(
-          'fixed top-0 z-[1010] flex h-full w-[18.75rem] grow-0 flex-col overflow-x-hidden border-r-0 bg-background-white-pale text-text-body transition-transform ease-in-out lg:bottom-0 lg:h-auto lg:translate-x-0 lg:border-r lg:border-r-border-muted-soft',
+          'bg-background-white-pale fixed top-0 z-[1010] flex h-full w-[18.75rem] grow-0 flex-col overflow-x-hidden border-r-0 text-text-body transition-transform ease-in-out lg:bottom-0 lg:h-auto lg:translate-x-0 lg:border-r lg:border-r-border-muted-soft',
           {
             '-translate-x-full': !open, //hide sidebar to the left when closed
           },
@@ -82,7 +82,7 @@ export function Sidebar({ items, brand }: SidebarProps) {
         >
           <div
             className={clsx(
-              'sticky top-0 bg-background-white-pale transition-shadow delay-0 duration-200 ease-[ease]',
+              'bg-background-white-pale sticky top-0 transition-shadow delay-0 duration-200 ease-[ease]',
               {
                 'shadow-[0_2px_5px_rgba(0,0,0,0.3)]': scrolled,
               },
