ddd064c51e0ccb1286d95cf45bc6b46120274afe
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-09-24 13:15:00 +1000

    fix

--- apps/protoform/src/app/page.tsx
+++ apps/protoform/src/app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
           leadText={
             <>
               <p className="mb-3">
-                The GEL Design System Forms Pattern Framework provides a simple, scalable approach for collecting
-                customer information. It includes standardised form validation and offers different navigational options
-                depending on the form’s length and complexity.
+                The GEL Forms Framework provides a simple, scalable approach for collecting customer information. It
+                includes standardised form validation and offers different navigational options depending on the form’s
+                length and complexity.
               </p>
               <p>
                 See examples below <i>(the content in these examples is for UI demonstration purposed only)</i>:
@@ -69,6 +69,11 @@ export default function Home() {
             <h3 className="typography-body-7 mb-5 font-bold text-heading xl:typography-body-6">
               Profile indicator examples
             </h3>
+            <p className="mb-5 typography-body-5 text-text">
+              When customers have multiple profiles (such as business and personal), indicators at the top of the form
+              clearly show which profile they are working in. The Extra-care indicator is used only for staff facing
+              processes.
+            </p>
             <List type="link" className="flex flex-col gap-1">
               <Link href="/personal-customer" passHref legacyBehavior>
                 <ListItem className="typography-body-9 flex flex-col gap-1">Personal customer</ListItem>
