b2840530244cc3a03c5f97cf9449bff6ebdf07a8
Jeremy Ortiz <jeremy.ortiz@westpac.com.au>
2025-09-25 15:11:44 +1000

    chore(ui): remove old tailwind 3 folder

--- packages/ui/src/tailwind/constants/breakpoints.ts
+++
@@ -1,8 +0,0 @@
-export const BREAKPOINTS = {
-  xsl: '576px',
-  sm: '768px',
-  md: '992px',
-  lg: '1200px',
-  xl: '1584px',
-};
-export type Breakpoint = keyof typeof BREAKPOINTS;
--- packages/ui/src/tailwind/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export { createSpacing } from './create-spacing.js';
-export { generateColorTints } from './generate-color-tints.js';
-export { createFontSizes } from './create-font-sizes.js';
-// export { generateFontComponents } from './generate-font-components.js';
-// export { generateFormControlComponents } from './generate-form-control.js';
-export { createBrandFonts } from './create-brand-fonts.js';
