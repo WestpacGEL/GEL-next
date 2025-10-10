1794d9685ad9a8f27c65c97877db8ed5ab24425f
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-10-10 17:30:03 +1000

    fix

--- apps/site/src/app/(home)/components/brand-select/components/list-box/components/list-box-option/list-box-option.styles.ts
+++ apps/site/src/app/(home)/components/brand-select/components/list-box/components/list-box-option/list-box-option.styles.ts
@@ -1,28 +1,25 @@
 import { tv } from 'tailwind-variants';
 
-export const styles = tv(
-  {
-    slots: {
-      base: 'border-t-border-muted-soft border-t first:border-t-0',
-      link: 'hover:bg-light focus:bg-light flex h-10 cursor-pointer items-center justify-between bg-white px-1 py-[0.625rem] transition-colors',
+export const styles = tv({
+  slots: {
+    base: 'border-t-border-muted-soft border-t first:border-t-0',
+    link: 'hover:bg-light focus:bg-light flex h-10 cursor-pointer items-center justify-between bg-white px-1 py-[0.625rem] transition-colors',
+  },
+  variants: {
+    isFocused: {
+      true: {
+        link: 'bg-light',
+      },
     },
-    variants: {
-      isFocused: {
-        true: {
-          link: 'bg-light',
-        },
+    isSelected: {
+      true: {
+        link: 'text-primary',
       },
-      isSelected: {
-        true: {
-          link: 'text-primary',
-        },
-      },
-      isDisabled: {
-        true: {
-          link: 'text-muted',
-        },
+    },
+    isDisabled: {
+      true: {
+        link: 'text-muted',
       },
     },
   },
-  { responsiveVariants: ['xsl', 'sm', 'md', 'lg', 'xl'] },
-);
+});
