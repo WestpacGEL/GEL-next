473929d285d2fa1f1aa9941e1dafec68f7d954b7
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-10-16 13:27:33 +1000

    upgrade repeater to be the same as compacta

--- packages/ui/src/components/repeater/repeater.types.ts
+++ packages/ui/src/components/repeater/repeater.types.ts
@@ -4,17 +4,13 @@ import { ResponsiveVariants } from 'src/types/responsive-variants.types.js';
 
 export type RepeaterProps = {
   /**
-   * Text for add button
+   * on add callback
+   */
+  onAdd?: () => unknown;
+  /**
+   * Add text
    */
   addText?: string;
-  /**
-   * Component to repeat
-   */
-  children: React.ReactNode;
-  /**
-   * Index heading tag to use for index on separator version
-   */
-  indexTag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   /**
    * Enable separator version
    */
