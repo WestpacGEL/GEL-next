09d77989535eded7d0e1de1476414705bc4a9f0d
Tom King <tom.jack.king@gmail.com>
2025-10-23 10:34:11 +1000

    - export ModalDialogFooterProps type

--- packages/ui/src/components/modal/components/modal-dialog/index.ts
+++ packages/ui/src/components/modal/components/modal-dialog/index.ts
@@ -1,2 +1,3 @@
 export { ModalDialog } from './modal-dialog.component.js';
 export { type ModalDialogProps } from './modal-dialog.types.js';
+export { type ModalDialogFooterProps } from './components/modal-dialog-footer/modal-dialog-footer.types.js';
