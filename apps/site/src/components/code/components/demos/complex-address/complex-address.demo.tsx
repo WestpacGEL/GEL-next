2e31b4306de1cc3e91ea77cdae196360b8cbc30e
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-10-24 16:39:55 +1000

    fix

--- apps/site/src/components/code/components/demos/complex-address/complex-address.demo.tsx
+++ apps/site/src/components/code/components/demos/complex-address/complex-address.demo.tsx
@@ -1,5 +1,5 @@
-import { Autocomplete, AutocompleteItem, Field, Form, FormGroup, Input, Select, SelectProps } from '@westpac/ui';
-import { Fragment, useState } from 'react';
+import { Autocomplete, AutocompleteItem, Field, Input, Select, SelectProps } from '@westpac/ui';
+import { useState } from 'react';
 
 const AddressManualComplexPattern = ({ property = 'house' }) => {
   let propertyStr;
@@ -27,59 +27,49 @@ const AddressManualComplexPattern = ({ property = 'house' }) => {
   }
 
   return (
-    <Fragment>
-      <FormGroup>
-        <Field label={propertyStr}>
+    <div className="flex flex-col gap-4">
+      <Field label={propertyStr}>
+        <Input size="large" className="w-full sm:w-3/12" />
+      </Field>
+      {extraStreetNum && (
+        <Field label="Street number">
           <Input size="large" className="w-full sm:w-3/12" />
         </Field>
-      </FormGroup>
-      {extraStreetNum && (
-        <FormGroup>
-          <Field label="Street number">
-            <Input size="large" className="w-full sm:w-3/12" />
-          </Field>
-        </FormGroup>
       )}
-      <FormGroup>
-        <Field label="Street name">
-          <Input size="large" className="w-full sm:w-8/12" />
-        </Field>
-      </FormGroup>
-      <FormGroup>
-        <Field label="Street type">
-          <Autocomplete size="large" className="w-full sm:w-5/12">
-            <AutocompleteItem key="select">Select</AutocompleteItem>
-            <AutocompleteItem key="street">Street</AutocompleteItem>
-            <AutocompleteItem key="road">Road</AutocompleteItem>
-            <AutocompleteItem key="avenue">Avenue</AutocompleteItem>
-          </Autocomplete>
-        </Field>
-      </FormGroup>
-      <FormGroup>
-        <Field label="Suburb">
-          <Input size="large" className="w-full sm:w-8/12" autoComplete="address-level2" />
-        </Field>
-      </FormGroup>
-      <FormGroup>
-        <Field label="State">
-          <Select size="large" className="w-full sm:w-5/12" autoComplete="address-level1">
-            <option>Select</option>
-            <option>NSW</option>
-            <option>VIC</option>
-            <option>QLD</option>
-            <option>ACT</option>
-            <option>SA</option>
-            <option>WA</option>
-            <option>NT</option>
-          </Select>
-        </Field>
-      </FormGroup>
-      <FormGroup>
-        <Field label="Postcode">
-          <Input size="large" className="w-full sm:w-2/12" autoComplete="postal-code" />
-        </Field>
-      </FormGroup>
-    </Fragment>
+      <Field label="Street name">
+        <Input size="large" className="w-full sm:w-8/12" />
+      </Field>
+
+      <Field label="Street type">
+        <Autocomplete size="large" className="w-full sm:w-5/12">
+          <AutocompleteItem key="select">Select</AutocompleteItem>
+          <AutocompleteItem key="street">Street</AutocompleteItem>
+          <AutocompleteItem key="road">Road</AutocompleteItem>
+          <AutocompleteItem key="avenue">Avenue</AutocompleteItem>
+        </Autocomplete>
+      </Field>
+
+      <Field label="Suburb">
+        <Input size="large" className="w-full sm:w-8/12" autoComplete="address-level2" />
+      </Field>
+
+      <Field label="State">
+        <Select size="large" className="w-full sm:w-5/12" autoComplete="address-level1">
+          <option>Select</option>
+          <option>NSW</option>
+          <option>VIC</option>
+          <option>QLD</option>
+          <option>ACT</option>
+          <option>SA</option>
+          <option>WA</option>
+          <option>NT</option>
+        </Select>
+      </Field>
+
+      <Field label="Postcode">
+        <Input size="large" className="w-full sm:w-2/12" autoComplete="postal-code" />
+      </Field>
+    </div>
   );
 };
 
@@ -99,13 +89,11 @@ export const ComplexAddressDemo = () => {
   const [property, setProperty] = useState<string>();
 
   return (
-    <Form spacing="large">
-      <FormGroup>
-        <Field label="Property type" hintMessage="Must be a residential address">
-          <PropertySelect value={property} onChange={e => setProperty(e.target.value)} />
-        </Field>
-      </FormGroup>
+    <form>
+      <Field label="Property type" hintMessage="Must be a residential address">
+        <PropertySelect value={property} onChange={e => setProperty(e.target.value)} />
+      </Field>
       {property && <AddressManualComplexPattern property={property} />}
-    </Form>
+    </form>
   );
 };
