e4f95a1ffbdfdb48a30d3e44b2fcfb85c6bfcd3d
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-10-27 16:02:21 +1000

    fix filter buttons

--- packages/ui/src/components/filter/components/filter-buttons/filter-buttons.styles.ts
+++ packages/ui/src/components/filter/components/filter-buttons/filter-buttons.styles.ts
@@ -3,8 +3,8 @@ import { tv } from 'tailwind-variants';
 export const styles = tv({
   slots: {
     base: 'relative flex justify-start',
-    buttonList: 'flex w-full touch-pan-x gap-1 overflow-x-auto overflow-y-visible whitespace-nowrap',
-    scrollButton: 'absolute rounded-none contrast-more:bg-black',
+    buttonList: 'flex w-full touch-pan-x gap-1 overflow-x-auto overflow-y-visible whitespace-nowrap py-1',
+    scrollButton: 'absolute top-0 bottom-0 h-auto rounded-none contrast-more:bg-black',
   },
   variants: {
     position: {
--- packages/ui/src/components/filter/filter.component.tsx
+++ packages/ui/src/components/filter/filter.component.tsx
@@ -4,7 +4,7 @@ import { type FilterProps } from './filter.types.js';
 
 export function Filter({ children, ...props }: FilterProps) {
   return (
-    <div {...props} className="flex flex-col gap-3">
+    <div {...props} className="flex flex-col gap-2">
       {children}
     </div>
   );
