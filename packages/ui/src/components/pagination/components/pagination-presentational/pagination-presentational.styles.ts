9bdd65401357a183b92a65a7078b04579ac5dd36
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-10-28 15:08:34 +1000

    fixes #1185 skip link focus

--- packages/ui/src/components/pagination/components/pagination-presentational/pagination-presentational.styles.ts
+++ packages/ui/src/components/pagination/components/pagination-presentational/pagination-presentational.styles.ts
@@ -3,7 +3,7 @@ import { tv } from 'tailwind-variants';
 export const styles = tv({
   slots: {
     base: 'flex flex-col items-center',
-    ul: 'flex overflow-hidden',
+    ul: 'flex',
     emptyItem:
       'typography-body-10 border-border-muted-soft text-text-body relative block min-w-7 cursor-default border-y px-2 py-1.5 text-center',
   },
