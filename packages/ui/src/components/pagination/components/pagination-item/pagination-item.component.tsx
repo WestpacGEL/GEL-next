bd8c3755d872eea8a32daaff88bfcb09317a341a
Kenji Shiroma <kenji.shiroma@westpac.com.au>
2025-10-28 13:53:16 +1000

    fixes #1184 pagination focus state

--- packages/ui/src/components/pagination/components/pagination-item/pagination-item.component.tsx
+++ packages/ui/src/components/pagination/components/pagination-item/pagination-item.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { styles } from './pagination-item.styles.js';
 import { type PaginationItemProps } from './pagination-item.types.js';
+import { AriaButtonOptions, mergeProps, useButton, useFocusRing } from 'react-aria';
 
 /**
  * @private
@@ -16,10 +17,16 @@ export function PaginationItem({
   disabled = false,
   ...props
 }: PaginationItemProps) {
+  let ref = React.useRef(null);
+  const { focusProps, isFocusVisible } = useFocusRing();
+  let { buttonProps } = useButton(props as AriaButtonOptions<'button'>, ref);
+
+  const finalButtonProps = mergeProps(focusProps, buttonProps);
+
   return (
     <Tag
-      className={styles({ className, firstItem, lastItem, active, disabled })}
-      {...props}
+      className={styles({ className, firstItem, lastItem, active, disabled, isFocusVisible })}
+      {...finalButtonProps}
       disabled={disabled}
       aria-disabled={disabled ? 'true' : 'false'}
       aria-current={active ? 'page' : undefined}
