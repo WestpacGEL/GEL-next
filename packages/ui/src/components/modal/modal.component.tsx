d1a9dd50983ec014216af42f94724c177ee3a5af
Tom King <tom.jack.king@gmail.com>
2025-11-10 11:45:54 +1000

    - added missing backdropStyle prop on Modal from master - removed the backdrop styling from the modal examples in docs site as styling for examples is different

--- packages/ui/src/components/modal/modal.component.tsx
+++ packages/ui/src/components/modal/modal.component.tsx
@@ -8,10 +8,10 @@ import { resolveResponsiveVariant } from '../../utils/breakpoint.util.js';
 import { ModalBackdrop, ModalDialog } from './components/index.js';
 import { type ModalProps } from './modal.types.js';
 
-export function Modal({ children, title, role, body, size, className, fullscreen, ...props }: ModalProps) {
+export function Modal({ children, backdropStyle, title, role, body, size, className, fullscreen, ...props }: ModalProps) {
   const breakpoint = useBreakpoint();
   return (
-    <ModalBackdrop size={resolveResponsiveVariant(size, breakpoint)} {...props}>
+    <ModalBackdrop size={resolveResponsiveVariant(size, breakpoint)} className={backdropStyle} {...props}>
       <ModalDialog
         fullscreen={fullscreen}
         onClose={props.isDismissable ? () => props.state.close() : undefined}
