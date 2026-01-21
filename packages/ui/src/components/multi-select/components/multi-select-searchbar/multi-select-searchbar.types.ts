b30578bf1a46d33f4dd9290a9edf6cdc695bf470
Tom King <tom.jack.king@gmail.com>
2026-01-22 09:54:03 +1000

    more lint fix

--- packages/ui/src/components/multi-select/components/multi-select-searchbar/multi-select-searchbar.types.ts
+++ packages/ui/src/components/multi-select/components/multi-select-searchbar/multi-select-searchbar.types.ts
@@ -1,8 +1,7 @@
 import { ButtonRef } from '../../../../components/button/button.types.js';
-import type { MultiSelectSize } from '../multi-select-list-box-trigger/multi-select-list-box-trigger.types.js';
 
-export interface MultiSelectSearchbarProps {
+export type MultiSelectSearchbarProps = {
   filterText: string;
   setFilterText: (text: string) => void;
   closeBtnRef: React.RefObject<ButtonRef>;
-}
+};
