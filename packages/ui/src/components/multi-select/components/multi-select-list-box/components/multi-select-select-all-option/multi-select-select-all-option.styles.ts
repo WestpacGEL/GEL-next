744a90e2ca0704cc87418b95e79a5b88e4032097
Tom King <tom.jack.king@gmail.com>
2026-02-04 08:46:47 +1000

    update indeterminate to hero

--- packages/ui/src/components/multi-select/components/multi-select-list-box/components/multi-select-select-all-option/multi-select-select-all-option.styles.ts
+++ packages/ui/src/components/multi-select/components/multi-select-list-box/components/multi-select-select-all-option/multi-select-select-all-option.styles.ts
@@ -6,7 +6,7 @@ export const styles = tv({
     button:
       'flex w-full cursor-pointer items-center gap-1 p-2 focus-visible:bg-background focus-visible:outline-2 focus-visible:!outline-offset-[-2px] focus-visible:focus-outline',
     checkbox: 'flex size-4 items-center justify-center rounded border border-hero bg-white',
-    indeterminate: 'block w-3/5 border-t-2 border-t-muted',
+    indeterminate: 'block w-3/5 border-t-2 border-t-hero',
     label: 'typography-body-9',
   },
   variants: {
