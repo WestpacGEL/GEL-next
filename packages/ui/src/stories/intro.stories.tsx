4c7dc844f76d500696d23d6267551d5e82a5aa83
Tom King <tom.jack.king@gmail.com>
2026-03-12 08:47:00 +1000

    update storybook landing page

--- packages/ui/src/stories/intro.stories.tsx
+++ packages/ui/src/stories/intro.stories.tsx
@@ -11,10 +11,15 @@ export default meta;
 
 export const Attention = () => (
   <div className="p-4">
-    <p className="typography-body-1">
+    <p className="typography-body-1 text-primary underline">Warning:</p>
+    <p className="typography-body-1 text-primary pb-6">
+      This Storybook is for version 0.x of GEL. It is highly recommended to upgrade to {'>'}=1.0.0 at the earliest
+      opportunity.
+    </p>
+    <p className="typography-body-7">
       This is a development environment only. Not all the components in this site are approved and ready for use. If you
       are looking for components that are approved by brand, UX, accessibility and GEL please see the{' '}
-      <Link href="https://gel.westpacgroup.com.au/design-system/wbc?b=WBC" type="inline">
+      <Link href="https://gel-next-site-git-version-0x-westpacgel.vercel.app/design-system/wbc" type="inline">
         Gel Design system
       </Link>
     </p>
