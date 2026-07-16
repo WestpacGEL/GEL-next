e1c7b99b31c97e59c7cd66ee258d3af502f222a2
Harleigh Gallant <harleigh.gallant@westpac.com.au>
2026-07-16 13:54:40 +1000

    Phone and tablet icon replaced

--- packages/ui/src/components/icon/components/phone-icon.tsx
+++ packages/ui/src/components/icon/components/phone-icon.tsx
@@ -7,9 +7,13 @@ export function PhoneIcon({ 'aria-label': ariaLabel = 'Phone', copyrightYear = '
   return (
     <Icon aria-label={ariaLabel} copyrightYear={copyrightYear} {...props}>
       <path
+        d="M13 4C13.5523 4 14 4.44772 14 5C14 5.55228 13.5523 6 13 6H11C10.4477 6 10 5.55228 10 5C10 4.44772 10.4477 4 11 4H13Z"
+        fill="currentColor"
+      />
+      <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M5 2C5 0.89543 5.89543 0 7 0H17C18.1046 0 19 0.895431 19 2V22C19 23.1046 18.1046 24 17 24H7C5.89543 24 5 23.1046 5 22V2ZM17 19H7V3H17V19ZM10.5 21.5C10.5 22.33 11.17 23 12 23C12.83 23 13.5 22.33 13.5 21.5C13.5 20.67 12.83 20 12 20C11.17 20 10.5 20.67 10.5 21.5Z"
+        d="M17 0C18.1046 0 19 0.895431 19 2V22C19 23.1046 18.1046 24 17 24H7C5.89543 24 5 23.1046 5 22V2C5 0.895431 5.89543 0 7 0H17ZM7 22H17V2H7V22Z"
         fill="currentColor"
       />
     </Icon>
