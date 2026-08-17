ac9a454e7d122bd6dbe2a8d4b1a1741d4f5c3036
Cheyenne Tonkins <cheyenne.tonkins@westpac.com.au>
2026-08-18 09:36:49 +1000

    Test flyout component in protoform

--- apps/protoform/src/components/sidebar/context.tsx
+++ apps/protoform/src/components/sidebar/context.tsx
@@ -7,20 +7,16 @@ type SidebarContextType = {
   open: boolean;
   ropeData?: ProgressRopeProps['data'];
   ropeStep: number;
-  sidebarScrolled: boolean;
   setOpen: (open: boolean) => unknown;
   setRopeData: (ropeData: ProgressRopeProps['data']) => unknown;
   setRopeStep: (ropeStep: number) => unknown;
-  setSidebarScrolled: (sidebarScrolled: boolean) => unknown;
 };
 
 export const useSidebar = create<SidebarContextType>(set => ({
   open: false,
   ropeData: undefined,
   ropeStep: 0,
-  sidebarScrolled: false,
   setOpen: (open: boolean) => set({ open }),
   setRopeData: (ropeData: ProgressRopeProps['data']) => set({ ropeData }),
   setRopeStep: (ropeStep: number) => set({ ropeStep }),
-  setSidebarScrolled: (sidebarScrolled: boolean) => set({ sidebarScrolled }),
 }));
